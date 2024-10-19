9ddb65ec4c1b6154829996857fa93707f4d769c1
Dharmendra Prajapati <158823688+dharmendra-coder1@users.noreply.github.com>
2024-10-19 16:24:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,14 +28,20 @@ gauth = GoogleAuth()
 drive = GoogleDrive(gauth)
 
 # MySQL database connection
+# import mysql.connector
+# import os
+
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT", 3306),  # Default to 3306 if not set
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME")
         )
+        if conn.is_connected():
+            print("Database connection successful")
         return conn
     except mysql.connector.Error as err:
         print(f"Error: {err}")
