88aa63e55793f6b21143283c17afbd0bd8a6821e
Dharmendra Prajapati <158823688+dharmendra-coder1@users.noreply.github.com>
2024-10-19 14:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,12 +29,13 @@ drive = GoogleDrive(gauth)
 
 # MySQL database connection (Updated with Render database details)
 
-mydb = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASS"),
-    database=os.getenv("DB_NAME")
-)
+def get_db_connection():
+    return mysql.connector.connect(
+       host=os.getenv("DB_HOST"),
+       user=os.getenv("DB_USER"),
+       password=os.getenv("DB_PASS"),
+       database=os.getenv("DB_NAME")'
+    )
 
 def log_uploaded_file(task_type, file_name, file_path):
     conn = get_db_connection()
